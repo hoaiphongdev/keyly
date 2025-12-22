2806ed20a0f041ccfc3abe658448129512d56bc5
hoaiphongdev <dev.hoaiphong@gmail.com>
2025-12-22 22:44:56 +0700

    ✨ feat: integrate Sparkle for automatic updates, add update manager, and enhance release process

--- Package.swift
+++ Package.swift
@@ -7,9 +7,15 @@ let package = Package(
     platforms: [
         .macOS(.v12)
     ],
+    dependencies: [
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.6.0")
+    ],
     targets: [
         .executableTarget(
             name: "Keyly",
+            dependencies: [
+                .product(name: "Sparkle", package: "Sparkle")
+            ],
             path: "Sources/Keyly",
             resources: [
                 .process("Resources")
